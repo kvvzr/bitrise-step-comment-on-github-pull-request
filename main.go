dd5f0f9dc2ccefbf5d756d4566d958759c295576
kwzr <kwzr@twinkrun.net>
2018-10-29 11:38:58 +0900

    First commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
+
+	//
+	// --- Step Outputs: Export Environment Variables for other Steps:
+	// You can export Environment Variables for other Steps with
+	//  envman, which is automatically installed by `bitrise setup`.
+	// A very simple example:
+	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
+	if err != nil {
+		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
+		os.Exit(1)
+	}
+	// You can find more usage examples on envman's GitHub page
+	//  at: https://github.com/bitrise-io/envman
+
+	//
+	// --- Exit codes:
+	// The exit code of your Step is very important. If you return
+	//  with a 0 exit code `bitrise` will register your Step as "successful".
+	// Any non zero exit code will be registered as "failed" by `bitrise`.
+	os.Exit(0)
+}
